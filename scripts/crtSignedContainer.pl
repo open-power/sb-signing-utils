33a461a64d450ee93b987b9603fdd61f3a4334c3
PAMELA J. EGGLER <eggler@gfwa609.aus.stglabs.ibm.com>
2016-11-07 14:26:31 -0600

    fixed stray character in the prolog

--- scripts/crtSignedContainer.pl
+++ scripts/crtSignedContainer.pl
@@ -7,7 +7,7 @@
 #
 # OpenPOWER sb-signing-utils Project
 #
-# Contributors Listed Below - COPYRIGHT 2016S
+# Contributors Listed Below - COPYRIGHT 2016
 # [+] International Business Machines Corp.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
