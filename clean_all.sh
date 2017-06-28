49cb1ccd0509d401dfb9577607e182ddd7be752a
Dave Heller <hellerda@us.ibm.com>
2017-06-28 14:07:18 -0400

    Begrudgingly add m4 directory

--- clean_all.sh
+++ clean_all.sh
@@ -10,7 +10,7 @@ case "`echo $1 | tr A-Z a-z`" in
     rm -f compile config.guess config.sub depcomp install-sh ltmain.sh missing
     rm -f config.status config.h config.h.in* config.log configure
     rm -f libtool Makefile.in Makefile stamp-h1
-    rm -rf m4/ autom4te.cache/ *.m4
+    rm -rf autom4te.cache/ *.m4
     ;;
   *)
     echo "Unknown build type: $1"
