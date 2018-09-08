794069b37bcb965bfa8bc91298c5266007b21c92
Dave Heller <hellerda@us.ibm.com>
2018-09-08 19:17:57 -0400

    Fix shellcheck warnings in build scripts
    Signed-off-by: Dave Heller <hellerda@us.ibm.com>

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 [ "$#" -gt "1" ] && echo "Too many args." && exit 1
 [ "$#" -lt "1" ] && set -- "$@" "lite"
 
-case "`echo $1 | tr A-Z a-z`" in
+case "$(echo "$1" | tr "[:upper:]" "[:lower:]")" in
   lite|light)
     cp -p config.h.lite config.h
     cp -p Makefile.lite Makefile
--- clean_all.sh
+++ clean_all.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 [ "$#" -gt "1" ] && echo "Too many args." && exit 1
 [ "$#" -lt "1" ] && set -- "$@" "lite"
 
-case "`echo $1 | tr A-Z a-z`" in
+case "$(echo "$1" | tr "[:upper:]" "[:lower:]")" in
   lite|light)
     make clean
     ;;
@@ -10,7 +12,7 @@ case "`echo $1 | tr A-Z a-z`" in
     rm -f compile config.guess config.sub depcomp install-sh ltmain.sh missing
     rm -f config.status config.h config.h.in* config.log configure
     rm -f libtool Makefile.in Makefile stamp-h1
-    rm -rf autom4te.cache/ *.m4
+    rm -rf -- autom4te.cache/ *.m4 m4/*
     ;;
   *)
     echo "Unknown build type: $1"
